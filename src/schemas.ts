0f68ef78f35f7f34a86ff838be4f916852d78ec7
rodel-pipc <rcrisosto@pic.net.ph>
2025-11-04 18:34:22 +0800

    chore(user): change user table name from user to users (#3)
    Co-authored-by: rodel-pipc <rcrisosto.pic.net.ph>

--- src/schemas.ts
+++ src/schemas.ts
@@ -1,9 +1,10 @@
 import { pgTable } from 'drizzle-orm/pg-core'
 
-export const user = pgTable('user', t => ({
+export const users = pgTable('users', t => ({
   id: t.serial().primaryKey(),
   name: t.text().notNull(),
   email: t.text().notNull().unique(),
+  password: t.text().notNull(),
   createdAt: t.timestamp().defaultNow().notNull(),
   updatedAt: t.timestamp().defaultNow().notNull()
 }))
